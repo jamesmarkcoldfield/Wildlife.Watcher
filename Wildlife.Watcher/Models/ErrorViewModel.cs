155929d87ccd6b437c2450a5c5ee0e72bdb35d9e
James Robertson <oatbake@hotmail.co.uk>
2025-11-11 08:32:59 +0000

    Initial Check-in

---
+++ Wildlife.Watcher/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Wildlife.Watcher.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
